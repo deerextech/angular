e674095d85ff7418b3347c648fbf5210289120eb
Danielle Stiring <deerextech@gmail.com>
2017-05-01 18:21:30 -0400

    forgot a not found route

--- short-link/client/main.js
+++ short-link/client/main.js
@@ -1,17 +1,21 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SignUpPage from './../imports/ui/SignUpPage';
-import Link from './../imports/ui/Link';
-
 import {Router, Route, browserHistory} from 'react-router';
 //using browser history because the urls are cleaner then hashhistory /#/page
+
+//components
+import SignUpPage from './../imports/ui/SignUpPage';
+import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
 
 const routes =
 (
   <Router history={browserHistory}>
     <Route exact path={'/links'} component={Link} />
     <Route path={'/signup'} component={SignUpPage} />
+    <Route path="*" component={NotFound} />
+
   </Router>
 );
 
---
+++ short-link/imports/ui/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class NotFound extends React.Component{
+  render(){
+    return <p>This is not the page you were looking for.  #404 </p>
+  }
+}
