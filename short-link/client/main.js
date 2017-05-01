51759547c75950fb61722bc794adb8fd5922d346
Danielle Stiring <deerextech@gmail.com>
2017-05-01 18:04:51 -0400

    Ripped out Blaze related templates and stuff I don't want
    Replaced with placeholder for first component
    
    Meteor is up and running
    Dependencies installed
    
    .. ready to start building

--- short-link/client/main.js
+++ short-link/client/main.js
@@ -1,22 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Meteor} from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {Router} from 'react-router';
 
-import './main.html';
+class SignupPage extends React.Component{
+  render(){
+    return <p>Sign up component shall go here</p>
+  }
+}
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Meteor.startup(() =>{
+  ReactDOM.render(<SignupPage />, document.getElementById('app'));
+})
