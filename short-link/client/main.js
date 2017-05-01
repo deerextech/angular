7aa3893d17da94b11e1fda3443e3d5554801c2a2
Danielle Stiring <deerextech@gmail.com>
2017-05-01 18:28:30 -0400

    one more empty component added.. can start building this proj out now

--- short-link/client/main.js
+++ short-link/client/main.js
@@ -8,13 +8,17 @@ import {Router, Route, browserHistory} from 'react-router';
 import SignUpPage from './../imports/ui/SignUpPage';
 import Link from './../imports/ui/Link';
 import NotFound from './../imports/ui/NotFound';
+import Login from './../imports/ui/Login';
 
 const routes =
 (
   <Router history={browserHistory}>
+    <Route exact path={'/'} component={Login} />
+
     <Route exact path={'/links'} component={Link} />
     <Route path={'/signup'} component={SignUpPage} />
     <Route path="*" component={NotFound} />
+    {/* * is a catch all, incase user tries to go to a url that doesn't exist  */}
 
   </Router>
 );
