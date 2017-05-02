a08664aced7e6797aa688a6c042b849e3bcebcf0
Danielle Stiring <deerextech@gmail.com>
2017-05-02 15:07:33 -0400

    Moved user validation into it's own folder/file.  import it in server main so it runs. Routes & validation are working again. Refactoring of mess main.js completed.
    Please enter the commit message for your changes. Lines starting
    	new file:   short-link/imports/api/users.js
    	modified:   short-link/server/main.js

--- short-link/server/main.js
+++ short-link/server/main.js
@@ -1,24 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import {Accounts} from 'meteor/accounts-base';
+import '../imports/api/users';
+//not getting exports so just importing file so it runs.
 
 Meteor.startup(() => {
-  Accounts.validateNewUser((user)=>{
-    const email = user.emails[0].address;
-    try{
-      new SimpleSchema({
-        email:{
-          type:String,
-          regEx:SimpleSchema.RegEx.Email
-        }
-      }).validate({email})
-    }
-    catch(e){
-      throw new Meteor.Error(400, e.message);
-    }
 
-    console.log('this is the user', user)
-    return true;
-  });
 
 });
